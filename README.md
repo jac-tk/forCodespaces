17bdd4aaf0a091e95f4a26f779388cc01f1bbf02
jac-tk <117427971+jac-tk@users.noreply.github.com>
2024-01-30 15:32:25 +0000

    update

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 test codespce
 
 @jac-tk ➜ /workspaces/forCodespaces (main) $ npm -v
-10.2.4
+10.2.4  
 @jac-tk ➜ /workspaces/forCodespaces (main) $ node -v
-v20.11.0
+v20.11.0  
+
+https://jac-tk.github.io/ForCodespace/webpush.html/myWebpush
--- setnode.sh
+++ setnode.sh
@@ -2,7 +2,7 @@
 
 #パッケージ更新、自動削除してキャッシュクリーンするやつ
 # sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
-sudo apt update && sudo apt npm -y && sudo apt autoremove -y && sudo apt autoclean -y
+sudo apt update && sudo apt nodejs,npm -y && sudo apt autoremove -y && sudo apt autoclean -y
 #パッケージ削除済みだか残留した設定ファイルをすべて削除するやつ
 sudo apt purge -y $(dpkg -l | grep '^rc' | awk '{print $2}')
 
